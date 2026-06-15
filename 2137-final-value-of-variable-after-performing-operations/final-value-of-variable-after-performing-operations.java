b7036c2ea61c86495c55bbab6394a59ddf1783a3
shinthya21-git <shinthya21@gmail.com>
2026-06-15 20:58:17 +0530

    Time: 1 ms (75.29%) | Memory: 44.4 MB (47.23%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X=0;
+        for(int i=0;i<operations.length;i++ )
+        if(operations[i].contains("++"))
+        {
+            X=X+1;
+        }
+        else
+        {
+            X=X-1;
+        }
+        return X;
+       
+    }
+}
